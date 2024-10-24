5626f75a89e23bb7c814fe3702cf09565d88f65d
MrAnas07 <anusamir45@gamil.com>
2024-10-24 11:35:05 +0500

    first

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='div'>
+        <h1>Muhammad Anas </h1>
+        <h1>Muhammad Amir</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
